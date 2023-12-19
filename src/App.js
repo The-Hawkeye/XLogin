35ffab9c4611dddd55b3681759f62c312e93a99b
The-Hawkeye <82801422+The-Hawkeye@users.noreply.github.com>
2023-12-20 01:52:50 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [username, setusername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [submit, setSubmit] = useState("");
+
+  const [click, setClick] = useState(false);
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    if(username ==="Crio"&&password==="password")
+    {
+      setSubmit("Welcome, user!");
+    }
+    else 
+    setSubmit("Invalid username or password");
+
+    setClick(true);
+  }
+
   return (
+  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <h1>Login page</h1>
+  {click ? (
+    <h4>{submit}</h4>
+  ) : (
+    <>
+      <form onSubmit={(e) => handleSubmit(e)}>
+       <div>
+          Username:
+          <input
+            label="Username"
+            type="text"
+            value={username}
+            onChange={(e) => setusername(e.target.value)}
+            required
+            placeholder="username"
+          />
+        </div>
+        <div>
+          Password:
+          <input
+            label="Password"
+            type="password" // Change type to "password" for password fields
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            pattern=".{8,}" // Example: Requires at least 8 characters
+            title="Password must be at least 8 characters long"
+            placeholder="password"
+          />
+        </div>
+        <button type="submit" >
+          Submit
+        </button>
+      </form>
+    </>
+  )}
+</div>
+
   );
 }
 
